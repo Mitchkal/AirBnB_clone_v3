30ace9d0ff4deba6eebbc04dcf94231bda25da3d
Peter <peterkasinamunda@gmail.com>
2024-01-09 06:14:10 +0000

    app update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,51 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-index
-"""
-
-from flask import jsonify
-from api.v1.views import app_views
-
-from models import storage
-
-
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
-def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
-
-
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
-=======
+ 
 """flask with routes"""
 
 from flask import jsonify
@@ -71,4 +25,3 @@ def counts():
             "users": storage.count("User")
     }
     return jsonify(class_counts)
->>>>>>> 91652a45b459eeeddd244b96a625b39a7fead9ac
